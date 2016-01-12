b66c377f1766e03517c517cb9f119c6dd289ead9
nic <netzeln@gmail.com>
2016-01-12 08:05:23 -0800

    few changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,10 @@
 $(document).ready(function(){
 
-  //-$("div").click(function(){
-  //  $(".clickIt").toggle();
-  //})
 
 
-
-//$(".term").click(function() {
-  //    $("#JavaScriptAnswer").toggle()
-    //  $("#JavaScript").toggle();
-
-//  });
-$("#JavaScript").click(function() {
-  $("#JavaScriptAnswer").show();
-});
-  //$("p").click(function() {
-      $("#Modulo").toggle();
-      $("#ModuloShowing").toggle();
-    });
-      $("#Operators").toggle();
-      $("#Nan").toggle();
-      $("#Variable Naming Conventions").toggle();
-      $("#+=").toggle();
-      $("#Function").toggle();
-      $("#Method").toggle();
-      $("#Argument to Method").toggle();
-      $("#String").toggle();
-      $("#Chaining Methods").toggle();
-      $("#Escape").toggle();
-      $("#Alert").toggle();
-      $("#Commenting Code").toggle();
-      $("#Boolean").toggle();
-      $("#Undefined").toggle();
-      $("#Parameter").togle();
-      $("#Return").toggle();
-
-    });
+  $(".term").click(function() {
+    $("#JavaScript").toggle();
+    $("#JavaScriptAnswer").toggle();
+  });
 
   });
