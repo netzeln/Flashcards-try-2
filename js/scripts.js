54bd8669ad47449a5bc0addff0b50e922759215b
nic <netzeln@gmail.com>
2016-01-12 07:45:56 -0800

    new try

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+  //-$("div").click(function(){
+  //  $(".clickIt").toggle();
+  //})
+
+
+
+//$(".term").click(function() {
+  //    $("#JavaScriptAnswer").toggle()
+    //  $("#JavaScript").toggle();
+
+//  });
+$("#JavaScript").click(function() {
+  $("#JavaScriptAnswer").show();
+});
+  //$("p").click(function() {
+      $("#Modulo").toggle();
+      $("#ModuloShowing").toggle();
+    });
+      $("#Operators").toggle();
+      $("#Nan").toggle();
+      $("#Variable Naming Conventions").toggle();
+      $("#+=").toggle();
+      $("#Function").toggle();
+      $("#Method").toggle();
+      $("#Argument to Method").toggle();
+      $("#String").toggle();
+      $("#Chaining Methods").toggle();
+      $("#Escape").toggle();
+      $("#Alert").toggle();
+      $("#Commenting Code").toggle();
+      $("#Boolean").toggle();
+      $("#Undefined").toggle();
+      $("#Parameter").togle();
+      $("#Return").toggle();
+
+    });
+
+  });
